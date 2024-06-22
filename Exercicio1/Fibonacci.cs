0553561cf009fc233d3f12afe4a09cff6a294d07
Elaine <elaine.almeida@al.infnet.edu.br>
2024-06-21 22:16:22 -0300

    Add project files.

---
+++ Exercicio1/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Fibonacci
+    {
+        private int _n1;
+        private int _n2;
+        private int _contador;
+
+        public int N1 { get => _n1; set => _n1 = value; }
+        public int N2 { get => _n2; set => _n2 = value; }
+        public int Contador { get => _contador; set => _contador = value; }
+
+        public int calcularFibonacci(int numeroDigitado)
+        {
+            int n1 = 0, n2 = 1, contador = 0;
+            for (int i = n2; i < numeroDigitado; i++)
+            {
+                contador = n1 + n2;
+                n1 = n2;
+                n2 = contador;
+
+                Console.WriteLine("Sequência de Fibonacci: " + contador);
+            }
+            return contador;
+
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número, abaixo mostrará a quantidade de números" +
+               " digitado, e então, exibirá os primeiros números da sequência de Fibonacci.: ");
+            int numeroDigitado = int.Parse(Console.ReadLine());
+
+           Fibonacci  exercicio1 = new Fibonacci();
+            int n1 = exercicio1.N1 = 0;
+            int n2 = exercicio1.N2 = 1;
+            Console.WriteLine("Sequência de Fibonacci: " + n1);
+            Console.WriteLine("Sequência de Fibonacci: " + n2);
+            exercicio1.calcularFibonacci(numeroDigitado);
+        }
+    }
+}
